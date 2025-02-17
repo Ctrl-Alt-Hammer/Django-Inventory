af387db64282fb8d093875c1ff748f16d5425e7f
Kevin <kevinlamboy@gmail.com>
2025-02-17 15:29:46 -0500

    created templates files

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views # import views from current directory
 
 urlpatterns = [
+    path('<int:item_id>/', views.detail, name='detail'),
     path('', views.index, name='index'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 def index(request):
     return HttpResponse("Hello, world. You're at the inventory index.")
+
+def detail(request, item_id):
+    return render(request, 'inventory/detail.html', {'item': Item.objects.get(pk=item_id)}) # render detail.html template with item context
